8856810e150cbec25c9b9b12442acd10724f5307
glmck13 <glmck13@verizon.net>
2019-01-05 13:07:04 -0500

    Add files via upload

---
+++ scripture.sh
@@ -0,0 +1,14 @@
+#!/bin/ksh
+
+URLBASE="https://mckserver.dyndns.org"
+VARBASE="/var/www/html"
+M3UFILE="./cdn/misc/scripture.m3u"
+
+print "http://ccc.usccb.org/cccradio/NABPodcasts/$(date +"%y_%m_%d").mp3" >$VARBASE/$M3UFILE
+
+cat - <<EOF
+<html><body>
+<p>Downloading $(date +"%B %d")</p>
+<audio controls><source src="$URLBASE/$M3UFILE"></audio>
+</body></html>
+EOF
