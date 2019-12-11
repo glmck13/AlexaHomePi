6ddb8f75596a1bd2523d5d7e164d2e8d8fbb466b
glmck13 <glmck13@verizon.net>
2019-12-11 16:54:40 -0500

    Add files via upload

---
+++ hoya_handler.py
@@ -0,0 +1,97 @@
+from lxml import html
+import requests
+import os
+
+def hoya_handler(event, context):
+
+    query = {}
+    speech = ''; audio = ''; stopplay = False
+    requesttype = event['request']['type']
+    shouldEndSession = True
+
+    if requesttype == "LaunchRequest":
+        speech = "Hoya Saxa! Ask me for a recap of our last game, or details about our upcoming game.  How can I help you?"
+        shouldEndSession = False
+
+    elif requesttype == "IntentRequest":
+        intent = event['request']['intent']
+        intentname = intent['name']
+        try:
+            slots = intent['slots']
+        except:
+            slots = {}
+
+        if intentname == "AskPi":
+            query['Intent'] = intentname
+            query['Trigger'] = "Hoya"
+            try:
+                query['Enum'] = slots['enum']['value']
+            except:
+                pass
+
+        elif intentname == "AMAZON.PauseIntent" or intentname == "AMAZON.CancelIntent" or intentname == "AMAZON.StopIntent":
+            stopplay = True
+
+        elif intentname == "AMAZON.ResumeIntent" or intentname == "AMAZON.HelpIntent":
+            pass
+
+        if query:
+            page = requests.get(os.environ.get('ALEXA_URL'), auth=(os.environ.get('ALEXA_USER'), os.environ.get('ALEXA_PASS')), params=query)
+            tree = html.fromstring(page.content)
+            speech = tree.xpath('//body/p/text()')[0]
+            try:
+                audio = tree.xpath('//body//audio/source/@src')[0]
+            except:
+                audio = ''
+
+    else:
+        speech = "Come back soon! Goodbye!"
+
+    response = {
+        "version": "1.0",
+        "sessionAttributes": {},
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": speech
+            },
+            "reprompt": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": ""
+                }
+            },
+            "card": {
+                "type": "Simple",
+                "title": "Hoya Saxa",
+                "content": ""
+            },
+            "shouldEndSession": shouldEndSession
+        }
+    }
+
+    if audio:
+        response['response']['card']['content'] = speech + " [" + audio + "] "
+        response['response']['directives'] = [
+            {
+            "type": "AudioPlayer.Play",
+            "playBehavior": "REPLACE_ALL",
+              "audioItem": {
+                "stream": {
+                  "token": audio,
+                  "url": audio,
+                "offsetInMilliseconds": 0
+                }
+              }
+            }
+        ]
+
+    if stopplay:
+        response['response']['directives'] = [
+            {
+            "type": "AudioPlayer.ClearQueue",
+            "clearBehavior": "CLEAR_ALL"
+            }
+        ]
+
+    return response
