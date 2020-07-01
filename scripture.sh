35e2ee712a8fa953b1afd56a845a180705b2fda2
glmck13 <glmck13@verizon.net>
2020-07-01 08:14:12 -0400

    Update scripture.sh

--- scripture.sh
+++ scripture.sh
@@ -4,7 +4,7 @@ URLBASE="https://mckserver.dyndns.org"
 VARBASE="/var/www/html"
 M3UFILE="./cdn/misc/scripture.m3u"
 
-print "http://ccc.usccb.org/cccradio/print "http://ccc.usccb.org/cccradio/NABPodcasts/$(date +"%Y/%Y-%m-%d")%20USCCB%20Daily%20Mass%20Readings.mp3" >$VARBASE/$M3UFILENABPodcasts/$(date +"%Y/%y_%m_%d").mp3" >$VARBASE/$M3UFILE
+print "http://ccc.usccb.org/cccradio/NABPodcasts/$(date +"%Y/%Y-%m-%d")%20USCCB%20Daily%20Mass%20Readings.mp3" >$VARBASE/$M3UFILE
 
 cat - <<EOF
 <html><body>
