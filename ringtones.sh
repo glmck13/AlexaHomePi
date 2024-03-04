09ece1aa9e5174ab18f7debc29e1ea8a5d1a74ea
glmck13 <glmck13@verizon.net>
2024-03-04 17:35:00 -0500

    Add files via upload

--- ringtones.sh
+++ ringtones.sh
@@ -1,43 +1,49 @@
 #!/bin/ksh
 
-PATH=$PATH:/usr/sbin:/usr/local/bin
+PATH=$HOME/bin:$PATH
 
 trap "" HUP INT QUIT
 
 CDRFILE=/tmp/cdr.txt
+TMPFILE=/tmp/cdr$$.txt
 RINGTONE=/tmp/ringtone.mp3
 RINGPID=/tmp/ringpid.txt
-CONFFILE=/usr/local/etc/ringtones.conf
-AWSCREDS=/usr/local/etc/awscreds.conf
-SOCKFILE=/var/run/asterisk/asterisk.ctl
+CONFFILE=~/etc/ringtones.conf
+AWSCREDS=~/etc/awscreds.conf
+#SOCKFILE=/var/run/asterisk/asterisk.ctl
 
-while [ \! -e $SOCKFILE ]
-do
-	sleep 10
-done
+#while [ \! -e $SOCKFILE ]
+#do
+#	sleep 10
+#done
 
 . $AWSCREDS
 
-asterisk -x 'core waitfullybooted'
-asterisk -x 'dialplan remove extension 6000@default'
-asterisk -x 'dialplan add extension 6000,1,System(/bin/echo\ \"${STRFTIME(${EPOCH},,%m%d%Y-%H:%M:%S)}\"\ \"${CALLERID(all)}${CALLBACK}\"\ >>/tmp/cdr.txt) into default replace'
-asterisk -x 'dialplan add extension 6000,2,Dial(DAHDI/1,20,r(info)) into default replace'
-asterisk -x 'dialplan add extension 6000,3,System(/bin/echo\ \"${STRFTIME(${EPOCH},,%m%d%Y-%H:%M:%S)}\"\ \"${DIALSTATUS}\"\ >>/tmp/cdr.txt) into default replace'
-asterisk -x 'dialplan add extension 6000,4,Set(GLOBAL(CALLBACK)=\"\") into default replace'
+#asterisk -x 'core waitfullybooted'
+#asterisk -x 'dialplan remove extension 6000@default'
+#asterisk -x 'dialplan add extension 6000,1,System(/bin/echo\ \"${STRFTIME(${EPOCH},,%m%d%Y-%H:%M:%S)}\"\ \"${CALLERID(all)}${CALLBACK}\"\ >>/tmp/cdr.txt) into default replace'
+#asterisk -x 'dialplan add extension 6000,2,Dial(DAHDI/1,20,r(info)) into default replace'
+#asterisk -x 'dialplan add extension 6000,3,System(/bin/echo\ \"${STRFTIME(${EPOCH},,%m%d%Y-%H:%M:%S)}\"\ \"${DIALSTATUS}\"\ >>/tmp/cdr.txt) into default replace'
+#asterisk -x 'dialplan add extension 6000,4,Set(GLOBAL(CALLBACK)=\"\") into default replace'
 
 >$CDRFILE; chmod 777 $CDRFILE
 
-tail -f $CDRFILE | while read cdr
+#tail -f $CDRFILE | while read cdr
+while true
 do
+	ncat -l 4573 </dev/null >$TMPFILE
+	now=$(date)
+	sed -e "s/^/$now|/" <$TMPFILE >>$CDRFILE
+	cdr=$(grep agi_callerid: $TMPFILE)
+	cdr=${cdr##* } cdr=${cdr##*<} cdr=${cdr%>*} cdr=${cdr//+1/}
+
 	[ -f $RINGPID ] && kill $(<$RINGPID) >/dev/null 2>&1
 	rm -f $RINGTONE $RINGPID
-
-	cdr=${cdr##*<} cdr=${cdr%>*}
 
 	[ ! "$cdr" ] && continue
 
 	[[ $cdr == 0* ]] && continue
-	
+
 	[[ $cdr == *NOANSWER ]] && continue
 
 	grep "^$cdr" $CONFFILE | IFS=, read number voice ringtone
@@ -49,7 +55,7 @@ do
 	fi
 
 	(
-		loop=3; while (( loop-- > 0 ))
+		loop=1; while (( loop-- > 0 ))
 		do
 			play $RINGTONE vol 2.0 >/dev/null 2>&1; sleep 3
 		done
